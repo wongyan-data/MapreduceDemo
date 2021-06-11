44ee839ef6c7f1063e84182a8724c87cfde6f30e
Travis <wy970418@163.com>
2021-06-11 14:57:11 +0800

    修改地址

---
+++ src/main/java/com/Travis/mapreduce/OutputFormat/LogReducer.java
@@ -0,0 +1,17 @@
+package com.Travis.mapreduce.OutputFormat;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class LogReducer extends Reducer<Text,NullWritable,Text,NullWritable>{
+
+    @Override
+    protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+        for (NullWritable value : values) {
+            context.write (key , NullWritable.get ());
+        }
+    }
+}
